e35c3cad46fcafeb57357226f3ea35dcfcb60b9a
dgrims3 <dgrims3@wgu.edu>
2022-01-19 20:00:59 +0000

    Second Commit

---
+++ src/controller/LoginController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private TextField loginUserName;
+
+    @FXML
+    private TextField loginPassword;
+
+    @FXML
+    void onActionLogin(ActionEvent event) {
+
+    }
+
+}
+
