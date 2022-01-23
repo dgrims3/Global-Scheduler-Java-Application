2f280a2562fd0e353ec66a7faf26b3740c2e084b
dgrims3 <dgrims3@wgu.edu>
2022-01-23 14:24:23 -0600

    Second Commit

--- src/controller/MainScreenController.java
+++ src/controller/MainScreenController.java
@@ -1,4 +1,141 @@
 package controller;
 
+        import javafx.fxml.FXML;
+        import javafx.scene.control.DatePicker;
+        import javafx.scene.control.RadioButton;
+        import javafx.scene.control.TableColumn;
+        import javafx.scene.control.TableView;
+        import javafx.scene.control.ToggleGroup;
+
 public class MainScreenController {
+
+    @FXML
+    private TableView<?> appointmentsTableView;
+
+    @FXML
+    private TableColumn<?, ?> apptID;
+
+    @FXML
+    private TableColumn<?, ?> apptTitle;
+
+    @FXML
+    private TableColumn<?, ?> apptDescription;
+
+    @FXML
+    private TableColumn<?, ?> apptLocation;
+
+    @FXML
+    private TableColumn<?, ?> apptContact;
+
+    @FXML
+    private TableColumn<?, ?> apptType;
+
+    @FXML
+    private TableColumn<?, ?> apptStartTime;
+
+    @FXML
+    private TableColumn<?, ?> apptEndTime;
+
+    @FXML
+    private TableColumn<?, ?> apptCustomerID;
+
+    @FXML
+    private TableColumn<?, ?> apptUserID;
+
+    @FXML
+    private RadioButton viewApptsByAllRdoBtn;
+
+    @FXML
+    private ToggleGroup ViewAppointmentsToggleGroup;
+
+    @FXML
+    private RadioButton viewApptsByWeekRdoBtn;
+
+    @FXML
+    private RadioButton viewApptsByMonthRdoBtn;
+
+    @FXML
+    private DatePicker viewApptsCalender;
+
+    @FXML
+    private TableView<?> customersTableView;
+
+    @FXML
+    private TableColumn<?, ?> custID;
+
+    @FXML
+    private TableColumn<?, ?> custFirstName;
+
+    @FXML
+    private TableColumn<?, ?> custLastName;
+
+    @FXML
+    private TableColumn<?, ?> custAddress;
+
+    @FXML
+    private TableColumn<?, ?> custZipCode;
+
+    @FXML
+    private TableColumn<?, ?> custPhoneNumber;
+
+    @FXML
+    void OnActionViewReportsScreen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionAddAppt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionAddCustomer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionCancelAppt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionDeleteCustomer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionLogOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionModifyAppt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionModifyCustomer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionViewApptsByAll(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionViewApptsByMonth(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionViewApptsByWeek(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionViewApptsCalender(ActionEvent event) {
+
+    }
+
 }
