78084432255374a838078ffdc1d3218ad4ed2bb1
dgrims3 <dgrims3@wgu.edu>
2022-01-26 22:21:41 -0600

    Second Commit

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -6,9 +6,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class LoginController {
 
-
+        ResourceBundle rb = ResourceBundle.getBundle("resourceBundle/rb", Locale.getDefault());
 
         @FXML
         private TextField loginPassword;
@@ -24,6 +27,17 @@ public class LoginController {
 
         @FXML
         private Button quitButton;
+
+
+        public void initialize() {
+                loginPassword.setText(rb.getString("Password"));
+                loginTitle.setText(rb.getString("Title"));
+                loginUserName.setText(rb.getString("UserName"));
+                loginButton.setText(rb.getString("Login"));
+                quitButton.setText(rb.getString("Quit"));
+
+        }
+
 
         @FXML
         void onActionLoginToMain(ActionEvent event) {
