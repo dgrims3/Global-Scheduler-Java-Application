cc33ee245cb4543ced6ddaa0dba4f67a7b5e4095
dgrims3 <dgrims3@wgu.edu>
2022-01-27 20:02:08 -0600

    Second Commit

--- src/DAO/UserDAO.java
+++ src/DAO/UserDAO.java
@@ -5,14 +5,34 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import model.User;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class UserDAO {
     public ObservableList <User> allUsers = FXCollections.observableArrayList();
-    public ObservableList <User> getAllUsers() {return allUsers;}
+    public void fillList(){
+        Statement statement = null;
+        ResultSet resultSet;
+        Connection connection = null;
+        String sql = "SELECT * FROM USERS;";
 
-    PreparedStatement ps;
-    ResultSet rs;
-    String query = "SELECT * FROM USERS;";
+        try {
+            connection = JDBC.getConnection();
+            statement.getConnection();
+            resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()){
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+        public ObservableList <User> getAllUsers() {return allUsers;}
 }
