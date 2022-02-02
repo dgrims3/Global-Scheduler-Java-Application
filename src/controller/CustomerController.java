627d5c6c5291453a7facd3b319efe130fb2973cb
dgrims3 <dgrims3@wgu.edu>
2022-02-02 06:24:07 -0600

    Second Commit

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class CustomerController {
+
+    @FXML private TextField addCustomerId;
+    @FXML private TextField addCustomerName;
+    @FXML private TextField addCustomerAddress;
+    @FXML private TextField addCustomerPostalCode;
+    @FXML private TextField addCustomerPhoneNumber;
+    @FXML private ComboBox<?> addCustomerCountryComboBox;
+    @FXML private ComboBox<?> addCustomerDivisionComboBox;
+
+    @FXML void onActionCancelAddCustomer(ActionEvent event) {
+
+    }
+    @FXML void onActionSaveAddCustomer(ActionEvent event) {
+
+    }
+    @FXML void onActionSelectCountry(ActionEvent event) {
+
+    }
+    @FXML void onActionSelectDivision(ActionEvent event) {
+
+    }
+
 }
