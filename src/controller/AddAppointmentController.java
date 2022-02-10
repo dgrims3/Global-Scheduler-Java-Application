6f5a0d7e22f40fddcc96a29dec79b88f4678af42
dgrims3 <dgrims3@wgu.edu>
2022-02-10 07:31:02 -0500

    test

---
+++ src/controller/AddAppointmentController.java
@@ -0,0 +1,49 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class AddAppointmentController {
+
+    @FXML
+    private TextField addCustomerAddress;
+
+    @FXML
+    private ComboBox<?> addCustomerCountryComboBox1;
+
+    @FXML
+    private TextField addCustomerId;
+
+    @FXML
+    private TextField addCustomerName;
+
+    @FXML
+    private TextField addCustomerPostalCode;
+
+    @FXML
+    private TextField addCustomerPostalCode1;
+
+    @FXML
+    private TextField addCustomerPostalCode2;
+
+    @FXML
+    private TextField addCustomerPostalCode3;
+
+    @FXML
+    void onActionCancelAddAppointment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionSaveAddAppointment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionSelectContact(ActionEvent event) {
+
+    }
+
+}
