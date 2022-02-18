ab7eb907acc6de524b957ff3a794aa3ee1e85828
dgrims3 <dgrims3@wgu.edu>
2022-02-17 16:10:09 -0800

    test

--- src/controller/AddAppointmentController.java
+++ src/controller/AddAppointmentController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import DAO.AppointmentDAO;
-import helper.TimeHelper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -14,17 +13,20 @@ import javafx.scene.control.TextField;
 import model.Appointment;
 import model.SceneChange;
 
+
 import java.io.IOException;
 import java.net.URL;
-import java.sql.Timestamp;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
+
 
 public class AddAppointmentController implements Initializable {
     AppointmentDAO dao = new AppointmentDAO();
     LocalDate date;
     Alert alert = new Alert(Alert.AlertType.WARNING);
+    ZoneId sysDef = ZoneId.systemDefault();
+    ZoneId EST = ZoneId.of("America/New_York");
     @FXML public DatePicker ApptDatePicker = new DatePicker();
     @FXML private TextField addAppointmentId;
     @FXML private ComboBox<String> addApptContact;
@@ -40,8 +42,8 @@ public class AddAppointmentController implements Initializable {
     public ObservableList<LocalTime> setTimeComboBox(){
         ObservableList<LocalTime> time = FXCollections.observableArrayList();
         LocalTime.now(ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("h:mm:ss"));
-        LocalTime start = LocalTime.of(8, 0);
-        LocalTime end = LocalTime.of(22, 0);
+        LocalTime start = LocalTime.of(5, 0);
+        LocalTime end = LocalTime.of(24, 0);
 
         while (start.isBefore(end.plusSeconds(1))) {
             time.add(start);
@@ -62,33 +64,37 @@ public class AddAppointmentController implements Initializable {
        SceneChange scene = new SceneChange();
         scene.changeScene(event, "/view/MainScreen.fxml");
     }
-    public boolean compareTimes(LocalDateTime local) {
-       ZoneId EST = ZoneId.of("US/Eastern");
-       ZonedDateTime zoned = ZonedDateTime.of(local, EST);
-       LocalDate date = LocalDate.of(local.getYear(), local.getMonth(), local.getDayOfMonth());
-       LocalTime openTime = LocalTime.of(8,0);
-       LocalTime closeTime = LocalTime.of(22, 0);
-       ZonedDateTime openHours = ZonedDateTime.of(date, openTime, EST);
-       ZonedDateTime closeHours = ZonedDateTime.of(date, closeTime, EST);
+    public boolean compareTimes(ZonedDateTime localStart, ZonedDateTime localEnd) {
+           ZonedDateTime zonedStart = ZonedDateTime.of(localStart.toLocalDateTime(), EST);
+           ZonedDateTime zonedEnd = ZonedDateTime.of(localEnd.toLocalDateTime(), EST);
+           LocalDateTime selectedStartTime = zonedStart.withZoneSameInstant(EST).toLocalDateTime();
+           LocalDateTime selectedEndTime = zonedEnd.withZoneSameInstant(EST).toLocalDateTime();
 
-       if(zoned.isBefore(openHours)){
+           ZonedDateTime openTime = (LocalDate.now().atTime(8, 0).atZone(EST));
+           ZonedDateTime closeTime = (LocalDate.now().atTime(22, 0).atZone(EST));
+           LocalDateTime bizOpenTime = openTime.withZoneSameInstant(EST).toLocalDateTime();
+           LocalDateTime bizCloseTime = closeTime.withZoneSameInstant(EST).toLocalDateTime();
+
+       if(selectedStartTime.isBefore(bizOpenTime)){
          alert.setContentText("Time selected is before 8:00 A.M. EST");
          alert.showAndWait();
          return false;
        }
-       else if (zoned.isAfter(closeHours)){
+       else if (selectedEndTime.isAfter(bizCloseTime)){
            alert.setContentText("Time selected is after 10:00 P.M. EST");
            alert.showAndWait();
            return false;
        }
-        System.out.println("success");
        return true;
     }
     @FXML void onActionSaveAddAppointment(ActionEvent event) {
         LocalDateTime start = LocalDateTime.of(date, apptHourPicker.getSelectionModel().getSelectedItem());
         LocalDateTime end = LocalDateTime.of(date, apptEndHourPicker.getSelectionModel().getSelectedItem());
+        ZonedDateTime zonedStart = LocalDateTime.of(date, apptHourPicker.getSelectionModel().getSelectedItem()).atZone(sysDef);
+        ZonedDateTime zonedEnd = LocalDateTime.of(date, apptEndHourPicker.getSelectionModel().getSelectedItem()).atZone(sysDef);
+
        try{
-           if (compareTimes(start)){
+           if (compareTimes(zonedStart, zonedEnd)){
                int appointment_ID = -1;
                String title = addApptTitle.getText();
                String description = addApptDescription.getText();
