9c432f88d357ec037b2d71a010bec834526423db
dgrims3 <dgrims3@wgu.edu>
2022-02-24 06:34:20 -0600

    test

--- src/controller/ReportsController.java
+++ src/controller/ReportsController.java
@@ -1,4 +1,58 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+
 public class ReportsController {
+
+    @FXML
+    private TableColumn<?, ?> apptContact;
+
+    @FXML
+    private TableColumn<?, ?> apptCustomerID;
+
+    @FXML
+    private TableColumn<?, ?> apptDescription;
+
+    @FXML
+    private TableColumn<?, ?> apptEndTime;
+
+    @FXML
+    private TableColumn<?, ?> apptID;
+
+    @FXML
+    private TableColumn<?, ?> apptStartTime;
+
+    @FXML
+    private TableColumn<?, ?> apptTitle;
+
+    @FXML
+    private TableColumn<?, ?> apptType;
+
+    @FXML
+    private TextArea avgDurationTextArea;
+
+    @FXML
+    private ComboBox<?> chooseContactComboBox;
+
+    @FXML
+    private TextArea numberOfApptsTextArea;
+
+    @FXML
+    private TableView<?> scheduleTableView;
+
+    @FXML
+    void onActionBackToMain(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionChooseContactComboBox(ActionEvent event) {
+
+    }
+
 }
